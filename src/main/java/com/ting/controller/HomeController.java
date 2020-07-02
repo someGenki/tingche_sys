d89c939ce596330c0cf96e750b500d5ebcfa07a1
小夕立 <40381052+someGenki@users.noreply.github.com>
2020-07-02 22:52:34 +0800

    '准备合并'

--- src/main/java/com/ting/controller/HomeController.java
+++ src/main/java/com/ting/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.ting.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ting.dao.ParkinfoMapper;
+import com.ting.pojo.Parkinfo;
+import com.ting.utils.ToJsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 主页跳转功能
@@ -9,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    @Autowired
+    ParkinfoMapper mapper;
+
     /**
      * 跳转到index.html
      *
@@ -17,5 +26,18 @@ public class HomeController {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    /**
+     * 获取车库信息
+     *
+     * @return
+     * @throws JsonProcessingException
+     */
+    @RequestMapping("/info")
+    @ResponseBody
+    public String getInfo() throws JsonProcessingException {
+        Parkinfo p = mapper.selectByPrimaryKey(1);
+        return ToJsonUtil.reJson(p);
     }
 }
